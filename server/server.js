5f4dcc0aed74f91330be45e4236d97c9fded36cf
Zaid-maker <pzhafeez@gmail.com>
2025-11-28 15:43:54 +0500

    structure

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-console.log("Server is starting...");
+const express = require('express');
+const app = express()
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
