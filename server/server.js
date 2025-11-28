f033c494525e43ed66247688e9be0f4c1db75e07
Zaid-maker <pzhafeez@gmail.com>
2025-11-28 15:47:40 +0500

    database

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
+const connectDB = require('./config/db');
+
 require('dotenv').config();
 
 const PORT = process.env.PORT;
 
 app.use(cors());
+
+// connect to database
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
